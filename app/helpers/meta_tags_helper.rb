f6c35033c7c91c23e9dd42ccde56b3eeadce35de
Alexey Solilin <solilin@jetrockets.com>
2024-02-15 10:31:11 +0100

    Tune meta tags

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -1,35 +1,50 @@
 module MetaTagsHelper
-  def site_title
-    "Company Name"
-  end
-
   def default_meta_tags
+    og_title = meta_tags["title"] || default_title
+    og_description = meta_tags["description"] || default_description
+
     {
-      title: site_title,
-      description: "Company description",
-      keywords: "Keywords",
-      canonical: @canonical || request.original_url,
+      site: "Site Name",
+      title: default_title,
+      description: default_description,
+      keywords: default_keywords,
+      canonical: request.original_url,
+      reverse: true,
       og: {
-        title: @page_title,
+        title: og_title,
         type: "website",
-        description: @page_description,
+        description: og_description,
         image: {
-          _: vite_asset_url_with_host("images/og-thumb.jpg"),
+          _: vite_asset_url_with_host("images/og.jpg"),
           width: 1200,
           height: 630
         },
         url: request.url,
-        site_name: site_title,
+        site_name: default_title,
         locale: :en_US
       },
       twitter: {
         card: "summary_large_image",
         site: "@twitter_account",
-        title: @page_title,
-        description: @page_description,
-        image: vite_asset_url_with_host("images/og-thumb.jpg"),
+        title: og_title,
+        description: og_description,
+        image: vite_asset_url_with_host("images/og.jpg"),
         url: request.url
       }
     }
+  end
+
+  private
+
+  def default_title
+    "Default Title"
+  end
+
+  def default_description
+    "Default Description"
+  end
+
+  def default_keywords
+    "Default keywords"
   end
 end
