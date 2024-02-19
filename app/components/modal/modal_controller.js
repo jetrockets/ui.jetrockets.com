57e0a9a2156f1ff2848c594da8670b8d3b00cbf0
Alexey Solilin <solilin@jetrockets.com>
2024-02-19 23:30:25 +0100

    Animation for modal

--- app/components/modal/modal_controller.js
+++ app/components/modal/modal_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from '@hotwired/stimulus'
+
 // import { Modal } from 'flowbite'
 import { stimulus } from '~/init'
 
@@ -10,11 +11,13 @@ export default class ModalController extends Controller {
       // backdropClasses: 'bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-40',
       closable: true,
       onHide: () => {
-        this.element.remove()
+        setTimeout(() => {
+          // this.element.remove()
+        }, 300)
       }
       // onShow: () => {
       //   console.log('modal is shown')
-      // },
+      // }
       // onToggle: () => {
       //   console.log('modal has been toggled')
       // }
