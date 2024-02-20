efd8b165e13d861325069d4d847ebe46c17b4dbe
Alexey Solilin <solilin@jetrockets.com>
2024-02-20 14:44:33 +0100

    Buttons

--- app/components/ui/badge/component.rb
+++ app/components/ui/badge/component.rb
@@ -1,5 +1,5 @@
 class Ui::Badge::Component < ApplicationComponent
-  TYPES = %i[default blue red green yellow indigo purple pink ]
+  TYPES = %i[default blue red green yellow indigo purple pink]
   DEFAULT_TYPE = :default
 
   def initialize(type: DEFAULT_TYPE, title:, **options)
@@ -15,8 +15,14 @@ class Ui::Badge::Component < ApplicationComponent
   def classes
     class_names(
       "text-sm font-medium px-2.5 py-0.5 rounded",
-      "bg-#{@type}-100 text-#{@type}-800": @type != :default,
-      "bg-gray-100 text-gray-800": @type == :default
+      "bg-gray-100 text-gray-800": @type == :default,
+      "bg-blue-100 text-blue-800": @type == :blue,
+      "bg-red-100 text-red-800": @type == :red,
+      "bg-green-100 text-green-800": @type == :green,
+      "bg-yellow-100 text-yellow-800": @type == :yellow,
+      "bg-indigo-100 text-indigo-800": @type == :indigo,
+      "bg-purple-100 text-purple-800": @type == :purple,
+      "bg-pink-100 text-pink-800": @type == :pink
     )
   end
 end
--- app/components/ui/dropdown/button_component.rb
+++ app/components/ui/dropdown/button_component.rb
@@ -1,8 +1,12 @@
 class Ui::Dropdown::ButtonComponent < ApplicationComponent
-  def initialize(title:, icon: true, **options)
+  def initialize(title: nil, icon: true, **options)
     @title = title
     @icon = icon
     @options = options
+  end
+
+  def call
+    helpers.component "ui/button", title: body, **attrs
   end
 
   private
@@ -11,17 +15,7 @@ class Ui::Dropdown::ButtonComponent < ApplicationComponent
     @options.deep_merge(data: { dropdown_toggle: "dropdown" })
   end
 
-  erb_template <<~ERB
-    <%= content_tag :button, type: "button", **attrs do %>
-      <span>
-        <%= @title %>
-      </span>
-
-      <% if @icon %>
-        <svg class="w-2.5 h-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4">
-        </svg>
-      <% end %>
-    <% end %>
-  ERB
+  def body
+    @title ? @title : content
+  end
 end
