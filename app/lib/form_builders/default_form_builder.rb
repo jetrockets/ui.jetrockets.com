1aabf71e9ac58a3dd675f55f65ec19286fe2e764
Alexey Solilin <solilin@jetrockets.com>
2024-03-19 15:58:00 +0100

    Add form builder

--- app/lib/form_builders/default_form_builder.rb
+++ app/lib/form_builders/default_form_builder.rb
@@ -52,68 +52,15 @@ module FormBuilders
       end
     end
 
-    #   # def radio_button(method, value, options = {})
-    #   #   input_classes = class_names(
-    #   #     "form__radio",
-    #   #     { "form__radio-errored": @object&.errors&.any },
-    #   #     options.delete(:class)
-    #   #   )
+    def group(options = {}, &block)
+      classes = class_names(
+        "form__group",
+        options&.delete(:class)
+      )
 
-    #   #   if options[:simple] == true
-    #   #     super(method, options.reverse_merge(class: input_classes))
-    #   #   else
-    #   #     group_options = options.delete(:group) || {}
-    #   #     group(group_options) do
-    #   #       label_options = options.delete(:label) || {}
-    #   #       @template.content_tag(:div, class: "flex items-start") do
-    #   #         @template.content_tag(:div, class: "leading-none") do
-    #   #           super(method, options.reverse_merge(class: input_classes))
-    #   #         end +
-    #   #         @template.content_tag(:div, class: "pl-2") do
-    #   #           label(method, label_options.merge({ class: "form__label-radio" })) +
-    #   #           @template.content_tag(:p, class: "form__hint") do
-    #   #             options[:hint]
-    #   #           end
-    #   #         end
-    #   #       end
-    #   #     end
-    #   #   end
-    #   # end
-
-
-    #   def label(method, options = {})
-    #     text = label_text(options)
-
-    #     classes = class_names(
-    #       "form__label",
-    #       options.is_a?(Hash) && options.delete(:class)
-    #     )
-
-    #     if options.is_a?(Hash)
-    #       super(method, text, options.merge({ class: classes }))
-    #     else
-    #       super(method, text, { class: classes })
-    #     end
-    #   end
-
-    #   def group(options = {}, &block)
-    #     classes = class_names(
-    #       "form__group",
-    #       options&.delete(:class)
-    #     )
-
-    #     @template.content_tag(:div, class: classes, **options) do
-    #       block.call
-    #     end
-    #   end
-
-    #   private
-
-    #   def label_text(options)
-    #     text = options[:text] if options.is_a?(Hash)
-    #     text ||= options if options.is_a?(String)
-    #     text
-    #     # || @object&.class&.human_attribute_name(method)
-    #   end
+      @template.content_tag(:div, class: classes, **options) do
+        block.call
+      end
+    end
   end
 end
