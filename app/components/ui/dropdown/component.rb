0da6704a9ebd5c391c0830795a56a12133d42d44
Alexey Solilin <solilin@jetrockets.com>
2024-03-19 16:41:02 +0100

    Tune dropdowns

--- app/components/ui/dropdown/component.rb
+++ app/components/ui/dropdown/component.rb
@@ -1,5 +1,4 @@
 class Ui::Dropdown::Component < ApplicationComponent
-  renders_one :button, Ui::Dropdown::ButtonComponent
   renders_many :links, Ui::Dropdown::LinksComponent
 
   def initialize(id:, **options)
@@ -7,14 +6,16 @@ class Ui::Dropdown::Component < ApplicationComponent
   end
 
   erb_template <<~ERB
-    <%= button %>
-
     <div id="<%= @id %>" class="dropdown">
-      <ul class="dropdown__wrapper">
-        <% links.each do |link| %>
-          <%= link %>
-        <% end %>
-      </ul>
+      <% if links.any? %>
+        <ul class="dropdown__wrapper">
+          <% links.each do |link| %>
+            <%= link %>
+          <% end %>
+        </ul>
+      <% else %>
+        <%= content %>
+      <% end %>
     </div>
   ERB
 end
