b2702bbe9bd26ded984ab5b736786fa65c44c2a5
Alexey Solilin <solilin@jetrockets.com>
2024-03-20 16:02:56 +0100

    Fix UI

--- app/lib/form_builders/default_form_builder.rb
+++ app/lib/form_builders/default_form_builder.rb
@@ -43,9 +43,9 @@ module FormBuilders
       label_text = options.delete(:label) || method.to_s.humanize
 
       @template.content_tag :label, class: "inline-flex items-center cursor-pointer" do
-        @template.concat @template.check_box(@object_name, method, options.merge(class: "peer hidden"), checked_value, unchecked_value)
-        @template.concat @template.content_tag(:div, "", class: "relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600")
-        @template.concat @template.content_tag(:span, label_text, class: "text-sm font-medium text-gray-900 ms-3 dark:text-gray-300")
+        @template.concat @template.check_box(@object_name, method, { class: "sr-only peer" }.merge(options), checked_value, unchecked_value)
+        @template.concat @template.content_tag(:div, "", class: "form__toggle")
+        @template.concat @template.content_tag(:span, label_text, class: "form__label form__label-checkbox")
       end
     end
 
