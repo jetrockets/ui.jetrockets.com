82f5000feeba26d28a9b0f1eee38d369bb922065
Alexey Solilin <solilin@jetrockets.com>
2025-02-18 11:22:21 +0100

    Remove safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,5 @@ module.exports = {
   plugins: [
     require('@tailwindcss/forms')(),
     require('flowbite/plugin')
-  ],
-  safelist: [
-    {
-      pattern: /btn-(xs|sm|md|lg|xl)/
-    }
   ]
 }
