e23cf5fd74050ca521874bb6a396edc44ca6ebf2
Julia Volkova <juliavolk96@gmail.com>
2025-02-20 18:07:36 +0400

    Add component slots

--- app/components/ui/card/component.rb
+++ app/components/ui/card/component.rb
@@ -1,13 +1,10 @@
 class Ui::Card::Component < ApplicationComponent
-  renders_one :header
-  renders_one :footer
+  renders_one :header, "Ui::Card::Header"
+  renders_one :footer, "Ui::Card::Footer"
   renders_one :body
 
-  ALIGN = %i[left center right]
-
-  def initialize( bordered: true, align: nil, **options)
+  def initialize( bordered: false, **options)
     @bordered = bordered
-    @align = align
     @options = options
   end
 
@@ -15,8 +12,37 @@ class Ui::Card::Component < ApplicationComponent
     class_names(
       "card",
       ("card-bordered" if @bordered),
-      "card-#{@align}",
       @options.delete(:class)
     )
+  end
+
+  class Ui::Card::Header < ApplicationComponent
+    attr_reader :classes
+
+    def initialize(classes: "card__header", **options)
+      @classes = classes
+      @options = options
+    end
+
+    def call
+      content_tag(:div, class: class_names("card__header", @classes), **@options) do
+        content
+      end
+    end
+  end
+
+  class Ui::Card::Footer < ApplicationComponent
+    attr_reader :classes
+
+    def initialize(classes: "card__footer", **options)
+      @classes = classes
+      @options = options
+    end
+
+    def call
+      content_tag(:div, class: class_names("card__footer", @classes), **@options) do
+        content
+      end
+    end
   end
 end
