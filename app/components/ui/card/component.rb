0764e14c59512aa873c054fadf4208267489c280
Julia Volkova <juliavolk96@gmail.com>
2025-02-20 13:20:47 +0400

    Fix card & styles

--- app/components/ui/card/component.rb
+++ app/components/ui/card/component.rb
@@ -2,4 +2,21 @@ class Ui::Card::Component < ApplicationComponent
   renders_one :header
   renders_one :footer
   renders_one :body
+
+  ALIGN = %i[left center right]
+
+  def initialize( bordered: true, align: nil, **options)
+    @bordered = bordered
+    @align = align
+    @options = options
+  end
+
+  def card_classes
+    class_names(
+      "card",
+      ("card-bordered" if @bordered),
+      "card-#{@align}",
+      @options.delete(:class)
+    )
+  end
 end
