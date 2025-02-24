6bf7c427c420aa8739356d65ad49615966749176
Julia Volkova <juliavolk96@gmail.com>
2025-02-24 20:23:36 +0400

    Fix table

--- app/components/ui/table/component.rb
+++ app/components/ui/table/component.rb
@@ -1,9 +1,24 @@
 class Ui::Table::Component < ApplicationComponent
-  attr_reader :headers, :rows
+  renders_many :rows, "Ui::Table::Row"
 
-  def initialize(headers: nil, rows: nil, **options)
+  attr_reader :headers
+
+  def initialize(headers: nil, **options)
     @headers = headers
-    @rows = rows
     @options = options
+  end
+
+  class Ui::Table::Row < ApplicationComponent
+    attr_reader :name, :email, :status, :created_at, :full_month, :edit_button
+
+    def initialize(name:nil, email: nil, status: nil, created_at: nil, full_month: nil, edit_button: nil, **options)
+      @name = name
+      @email = email
+      @status = status
+      @created_at = created_at
+      @full_month = full_month
+      @edit_button = edit_button
+      @options = options
+    end
   end
 end
