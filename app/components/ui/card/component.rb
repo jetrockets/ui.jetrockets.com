b5ffeb16ac0d47f8d8444b2d333d694a13ba9ef6
Alexey Solilin <solilin@jetrockets.com>
2025-02-25 16:59:01 +0100

    Fix card

--- app/components/ui/card/component.rb
+++ app/components/ui/card/component.rb
@@ -1,56 +1,13 @@
 class Ui::Card::Component < ApplicationComponent
-  renders_one :header, "Ui::Card::Header"
-  renders_one :footer, "Ui::Card::Footer"
+  renders_one :header, "Ui::Card::Header::Component"
+  renders_one :footer, "Ui::Card::Footer::Component"
   renders_one :body
 
-  def initialize( bordered: false, **options)
-    @bordered = bordered
+  def initialize(**options)
     @options = options
   end
 
-  def card_classes
-    class_names(
-      "card",
-      ("card-bordered" if @bordered),
-      @options.delete(:class)
-    )
-  end
+  private
 
-  class Ui::Card::Header < ApplicationComponent
-    def initialize(align: :left, **options)
-      @align = align
-      @options = options
-    end
-
-    def call
-      content_tag(:div, content, class: class_names("card__header", align_class), **@options)
-    end
-
-    def align_class
-      {
-        left: "items-start",
-        center: "items-center",
-        right: "items-end"
-      }[@align]
-    end
-  end
-
-  class Ui::Card::Footer < ApplicationComponent
-    def initialize(align: :right, **options)
-      @align = align
-      @options = options
-    end
-
-    def call
-      content_tag(:div, content, class: class_names("card__footer", align_class), **@options)
-    end
-
-    def align_class
-      {
-        left: "items-start",
-        center: "items-center",
-        right: "items-end"
-      }[@align]
-    end
-  end
+  attr_reader :options
 end
---
+++ app/components/ui/card/header/component.rb
@@ -0,0 +1,33 @@
+class Ui::Card::Header::Component < ApplicationComponent
+  def initialize(title: nil, align: :left, bordered: true, **options)
+    @title = title
+    @align = align
+    @bordered = bordered
+    @options = options
+  end
+
+  def call
+    content_tag(:div, class: classes, **@options) do
+      concat content_tag(:strong, @title, class: "card__title") if @title.present?
+      concat content
+    end
+  end
+
+  private
+
+  def classes
+    class_names(
+      "card__header",
+      align_class,
+      "card__header-bordered": @bordered
+    )
+  end
+
+  def align_class
+    {
+      left: "items-start",
+      center: "items-center",
+      right: "items-end"
+    }[@align]
+  end
+end
