5418acfb2d7aa4d778b88bdecbc37db6a0211c42
Julia Volkova <juliavolk96@gmail.com>
2025-02-28 19:17:27 +0400

    Add button group preview

---
+++ test/components/previews/ui/btn_group/component_preview.rb
@@ -0,0 +1,17 @@
+class Ui::BtnGroup::ComponentPreview < ViewComponent::Preview
+  def default
+    render(Ui::BtnGroup::Component.new) do |component|
+      component.with_button(variant: :primary, outlined: true) { "Button First" }
+      component.with_button(variant: :primary, outlined: true) { "Button Second" }
+      component.with_button(variant: :primary, outlined: true) { "Button Third" }
+    end
+  end
+
+  def with_gap
+    render(Ui::BtnGroup::Component.new(with_gap: true)) do |component|
+      component.with_button(variant: :primary, outlined: true) { "Button With Gap" }
+      component.with_button(variant: :primary, outlined: true) { "Button With Gap" }
+      component.with_button(variant: :primary, outlined: true) { "Button With Gap" }
+    end
+  end
+end
