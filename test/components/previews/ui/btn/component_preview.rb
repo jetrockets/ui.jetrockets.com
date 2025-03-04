615aac375caaf35d16d41e28e698b896eb5afde7
Julia Volkova <juliavolk96@gmail.com>
2025-03-04 19:53:15 +0400

    Fix btn preview

--- test/components/previews/ui/btn/component_preview.rb
+++ test/components/previews/ui/btn/component_preview.rb
@@ -1,45 +1,30 @@
 class Ui::Btn::ComponentPreview < ViewComponent::Preview
-  def default
-    render(Ui::Btn::Component.new.with_content("Default"))
+  # @param content text
+  # @param variant select :variant_options
+  # @param size select :size_options
+  # @param rounded toggle
+  # @param block toggle
+  # @param circle toggle
+  # @param outlined toggle
+  # @param href
+  # @param icon
+
+  def default(content: "Button", variant: :default, size: :md, rounded: false, block: false, circle: false, outlined: false, href: nil, icon: nil)
+    render(Ui::Btn::Component.new(variant:, size:, rounded:, block:, circle:, outlined:, href:).with_content(content).with_icon_content(icon))
   end
 
-  def primary
-    render(Ui::Btn::Component.new(variant: :primary).with_content("Primary"))
+  private
+
+  def variant_options
+    {
+      choices: %i[primary secondary danger ghost link],
+      include_blank: :default
+    }
   end
 
-  def secondary
-    render(Ui::Btn::Component.new(variant: :secondary).with_content("Secondary"))
-  end
-
-  def danger
-    render(Ui::Btn::Component.new(variant: :danger).with_content("Danger"))
-  end
-
-  def ghost
-    render(Ui::Btn::Component.new(variant: :ghost).with_content("Ghost"))
-  end
-
-  def link
-    render(Ui::Btn::Component.new(variant: :link).with_content("Link"))
-  end
-
-  def rounded
-    render(Ui::Btn::Component.new(variant: :primary, rounded: true).with_content("Rounded"))
-  end
-
-  def with_icon
-    render(Ui::Btn::Component.new.with_content("Default").with_icon_content("images/icons/arrow.svg"))
-  end
-
-  def outlined
-    render(Ui::Btn::Component.new(outlined: true).with_content("Outlined"))
-  end
-
-  def circle
-    render(Ui::Btn::Component.new(circle: true, size: :xs).with_icon_content("images/icons/arrow.svg"))
-  end
-
-  def block
-    render(Ui::Btn::Component.new(block: true, variant: :primary, size: :lg).with_content("Block Button"))
+  def size_options
+    {
+      choices: %i[xs sm md lg xl]
+    }
   end
 end
