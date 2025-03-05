0473d4e7cda5f1891585371155bb744256e637e5
Julia Volkova <juliavolk96@gmail.com>
2025-03-05 18:38:51 +0400

    Fix card preview

--- test/components/previews/ui/card/component_preview.rb
+++ test/components/previews/ui/card/component_preview.rb
@@ -1,29 +1,54 @@
 class Ui::Card::ComponentPreview < ViewComponent::Preview
-  def button_left
-    render Ui::Card::Component.new do |component|
-      component.with_header align: :left, title: "Noteworthy technology acquisitions 2021"
+  # @param header_title
+  # @param body_content
+  # @param header_bordered toggle
+  # @param footer_bordered toggle
+  # @param header_align select :header_align_options
+  # @param footer_align select :footer_align_options
+  # @param button_content
+  # @param variant select :variant_options
 
-      component.with_body do
-        content_tag(:p, "This is the body of the card. You can put any content here.")
+  def default(
+    header_title: "Noteworthy technology acquisitions 2021",
+    header_align: :left,
+    header_bordered: true,
+    body_content: "This is the body of the card. You can put any content here.",
+    footer_align: :right,
+    footer_bordered: true,
+    variant: :primary,
+    button_content: "Button"
+  )
+    render(Ui::Card::Component.new) do |card|
+      card.with_header( title: header_title, align: header_align, bordered: header_bordered)
+
+      card.with_body do
+        content_tag(:p, body_content)
       end
 
-      component.with_footer(align: :left) do |footer|
-        footer.render Ui::Btn::Component.new(variant: :primary).with_content("Primary")
+      card.with_footer(align: footer_align, bordered: footer_bordered) do |footer|
+        footer.render(Ui::Btn::Component.new(variant: variant).with_content(button_content))
       end
     end
   end
 
-  def button_right
-    render Ui::Card::Component.new do |component|
-      component.with_header align: :left, title: "Noteworthy technology acquisitions 2021"
+  private
 
-      component.with_body do
-        content_tag(:p, "This is the body of the card. You can put any content here.")
-      end
+  def header_align_options
+    {
+      choices: %i[left center right]
+    }
+  end
 
-      component.with_footer(align: :right) do |footer|
-        footer.render Ui::Btn::Component.new(variant: :secondary).with_content("Secondary")
-      end
-    end
+  def footer_align_options
+    {
+      choices: %i[left center right]
+    }
+  end
+
+  def variant_options
+    {
+      choices: %i[primary secondary danger ghost link],
+      include_blank: "default"
+    }
   end
 end
