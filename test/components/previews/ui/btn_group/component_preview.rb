b392e20c18ef51d6ffbd9e12ae7be776b27b8da3
Julia Volkova <juliavolk96@gmail.com>
2025-03-05 13:23:50 +0400

    Fix btn_group preview

--- test/components/previews/ui/btn_group/component_preview.rb
+++ test/components/previews/ui/btn_group/component_preview.rb
@@ -1,16 +1,39 @@
 class Ui::BtnGroup::ComponentPreview < ViewComponent::Preview
-  def default
-    render(Ui::BtnGroup::Component.new) do |component|
-      component.with_button(variant: :primary, outlined: true) { "Button Second" }
-      component.with_button(variant: :primary, outlined: true) { "Button Third" }
+  # @param content text
+  # @param button_count select :button_count_options
+  # @param variant select :variant_options
+  # @param size select :size_options
+  # @param with_gap toggle
+  # @param outlined toggle
+  # @param href
+  # @param icon
+
+  def default(content: "Button", button_count: 3, variant: :primary, size: :md, with_gap: false, outlined: true, href: nil, icon: nil)
+    render(Ui::BtnGroup::Component.new(with_gap: )) do |component|
+      button_count.to_i.times do |i|
+        component.with_button(variant:, size:, outlined:, href: ).with_content(content).with_icon_content(icon)
+      end
     end
   end
 
-  def with_gap
-    render(Ui::BtnGroup::Component.new(with_gap: true)) do |component|
-      component.with_button(variant: :primary, outlined: true) { "Button With Gap" }
-      component.with_button(variant: :primary, outlined: true) { "Button With Gap" }
-      component.with_button(variant: :primary, outlined: true) { "Button With Gap" }
-    end
+  private
+
+  def variant_options
+    {
+      choices: %i[primary secondary danger ghost link],
+      include_blank: :default
+    }
+  end
+
+  def size_options
+    {
+      choices: %i[xs sm md lg xl]
+    }
+  end
+
+  def button_count_options
+    {
+      choices: [2, 3, 4, 5]
+    }
   end
 end
