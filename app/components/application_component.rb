fa9f948205469f08a6aab45bfca48314910b4757
Valeryia Shyrko <valerka.shirko@gmail.com>
2025-03-18 18:00:14 +0100

    Add typography

--- app/components/application_component.rb
+++ app/components/application_component.rb
@@ -1,3 +1,12 @@
 class ApplicationComponent < ViewComponent::Base
   include Turbo::FramesHelper
+
+  def initialize(cont: nil, **options)
+    @cont = cont
+    @options = options
+  end
+
+  def call
+    @cont.html_safe
+  end
 end
