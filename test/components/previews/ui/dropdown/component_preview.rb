9317cb8977456b700128719b7009b19227b01146
Valeryia Shyrko <valerka.shirko@gmail.com>
2025-03-26 13:06:47 +0100

    Add dropdown to lookbook

---
+++ test/components/previews/ui/dropdown/component_preview.rb
@@ -0,0 +1,45 @@
+class Ui::Dropdown::ComponentPreview < ViewComponent::Preview
+  # @param trigger_title
+  # @param menu_title
+  # @param item_content
+  # @param link_title
+  # @param link_href
+  # @param placement
+  # @param triggerType
+  # @param offsetSkidding number
+  # @param offsetDistance number
+  # @param delay number
+  # @param ignoreClickOutsideClass toggle
+
+  def default(
+    trigger_title: "Dropdown",
+    menu_title: "Title",
+    item_content: "Any content",
+    link_title: "Settings",
+    link_href: "#",
+    placement: "right",
+    triggerType: "hover",
+    offsetSkidding: 130,
+    offsetDistance: 10,
+    delay: 300,
+    ignoreClickOutsideClass: false
+    )
+
+    render(Ui::Dropdown::Component.new(data: { placement: placement, triggerType: triggerType, offsetSkidding: offsetSkidding, offsetDistance: offsetDistance, delay: delay, ignoreClickOutsideClass: ignoreClickOutsideClass })) do |dropdown|
+      dropdown.with_trigger(class: "btn btn-primary btn-iconed") do
+        trigger_title
+      end
+
+      dropdown.with_menu do |list|
+        list.with_title { menu_title }
+
+        list.with_element_item do
+          item_content
+        end
+
+        list.with_element_divider
+        list.with_element_link(title: link_title, href: link_href)
+      end
+    end
+  end
+end
