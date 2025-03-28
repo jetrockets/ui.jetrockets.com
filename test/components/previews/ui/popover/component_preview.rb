059c5352bbb6d28ad52544f8f002e0cb0c62dae7
Valeryia Shyrko <valerka.shirko@gmail.com>
2025-03-28 12:04:08 +0100

    Add popover to lookbook

---
+++ test/components/previews/ui/popover/component_preview.rb
@@ -0,0 +1,41 @@
+class Ui::Popover::ComponentPreview < ViewComponent::Preview
+  # @param trigger_title
+  # @param popover_title
+  # @param popover_content
+  # @param placement select :placement_options
+  # @param triggerType select :trigger_options
+  # @param offset number
+
+  def default(
+    trigger_title: "Default popover",
+    popover_title: "Popover Title",
+    popover_content: "And here's some amazing content. It's very engaging.",
+    placement: :bottom,
+    triggerType: :click,
+    offset: 0
+  )
+
+    render(Ui::Popover::Component.new(data: { placement: placement, triggerType: triggerType, offset: offset })) do |popover|
+      popover.with_trigger(class: "btn mr-2") do
+        trigger_title
+      end
+      popover.with_menu(title: popover_title) do
+        content_tag(:p, popover_content)
+      end
+    end
+  end
+
+  private
+
+  def placement_options
+    {
+      choices: %i[bottom left right top]
+    }
+  end
+
+  def trigger_options
+    {
+      choices: %i[click hover none]
+    }
+  end
+end
