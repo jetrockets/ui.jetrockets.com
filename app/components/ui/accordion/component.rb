11a1b327af47852238da203065afcad3384efd88
Valeria Shirko <62662347+lera-sh@users.noreply.github.com>
2025-03-28 09:03:04 +0100

    Add accordion-controller (#18)
    * Add accordion-controller
    
    * Fix accordion targets
    
    * Fix inaccuracies
    
    * Add accordion to lookbook
    
    * Fix accordion lookbook props
    
    * Fix arrow rotation

--- app/components/ui/accordion/component.rb
+++ app/components/ui/accordion/component.rb
@@ -1,11 +1,25 @@
 class Ui::Accordion::Component < ApplicationComponent
   renders_many :items, Ui::Accordion::ItemComponent
 
+  def initialize(**options)
+    super
+    @options = options
+  end
+
   erb_template <<~ERB
-    <div class="accordion" data-accordion="collapse" data-active-classes="bg-gray-50">
-      <% items.each do |item| %>
-        <%= item %>
-      <% end %>
-    </div>
+    <%= content_tag :div, **attrs do %>
+      <div class="accordion" data-accordion="collapse">
+        <% items.each do |item| %>
+          <%= item %>
+        <% end %>
+      </div>
+    <% end %>
   ERB
+
+  private
+
+  def attrs
+    data_attributes = ({ controller: "accordion" }).deep_merge(@options.fetch(:data, {}))
+    @options.merge(data: data_attributes)
+  end
 end
--- app/components/ui/accordion/item_component.rb
+++ app/components/ui/accordion/item_component.rb
@@ -4,17 +4,19 @@ class Ui::Accordion::ItemComponent < ApplicationComponent
     @title = title
     @opened = opened
     @options = options
+    @options[:data] ||= {}
+    @options[:data][:accordion_target] = "item"
   end
 
   erb_template <<~ERB
-    <button type="button" class="accordion__header" data-accordion-target="#accordion_<%= @id %>" aria-expanded="<%= true if @opened %>">
-    <span>
-      <%= @title %>
-    </span>
-    <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5">
-    </svg>
-  </button>
+    <button type="button" class="accordion__header" data-accordion-target="trigger" data-accordion-id="<%= @id %>" aria-expanded="<%= @opened %>">
+      <span>
+        <%= @title %>
+      </span>
+      <svg data-accordion-icon class="w-3 h-3 transition-transform duration-300 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
+      </svg>
+    </button>
 
     <%= item %>
   ERB
