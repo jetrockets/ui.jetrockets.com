f87469db4740d625ded38544b7a89e200b42d404
Valeryia Shyrko <valerka.shirko@gmail.com>
2025-04-21 12:57:11 +0200

    Tune controller

--- app/components/ui/tag_input/tagify_controller.js
+++ app/components/ui/tag_input/tagify_controller.js
@@ -4,25 +4,72 @@ import { stimulus } from '~/init'
 import '@yaireo/tagify/dist/tagify.css'
 
 export default class TagifyController extends Controller {
+  get tagifyInstance () {
+    const { mode, whitelist, maxtags } = this.element.dataset
+
+    return new Tagify(this.element, {
+      tagTextProp: 'name',
+      editTags: false,
+      mode: mode || null,
+      dropdown: {
+        enabled: 0,
+        maxItems: 100,
+        placeAbove: false,
+        fuzzySearch: false
+      },
+      whitelist: whitelist?.split(',') || '[]',
+      maxTags: maxtags || 100
+    })
+  }
+
   connect () {
-    this.tagify = new Tagify(this.element, this.#options())
+    this.tagify = this.tagifyInstance
+    this.#setupInputListener()
+    this.tagify.on('dropdown:select', this.#onSelectSuggestion.bind(this))
+    this.tagify.setContentEditable(!this.tagify.hasMaxTags())
+    this.tagify.on('change', () => {
+      this.tagify.setContentEditable(!this.tagify.hasMaxTags())
+      this.dispatch('tagifyChange', { detail: { tagify: this.tagify } })
+    })
   }
 
   disconnect () {
-    this.tagify.destroy()
+    this.tagify.off('input')
   }
 
-  #options () {
-    const { whitelist, maxitems, closeonselect } = this.element.dataset
+  reload () {
+    this.disconnect()
+    this.connect()
+  }
 
-    return {
-      whitelist: whitelist?.split(',') || [],
-      dropdown: {
-        enabled: 0,
-        maxItems: maxitems || 10,
-        classname: 'tags-look',
-        closeOnSelect: (/true/).test(closeonselect) || false
+  #setupInputListener () {
+    let controller = new AbortController()
+
+    this.tagify.on('input', (event) => {
+      const path = this.element.dataset.tagsPath
+
+      if (path) {
+        const { value } = event.detail
+        this.tagify.settings.whitelist.length = 0
+
+        controller.abort()
+        controller = new AbortController()
+        this.tagify.loading(true)
+
+        fetch(`${path}?query=${value}`, { signal: controller.signal })
+          .then((response) => response.json())
+          .then((whitelist) => {
+            this.tagify.whitelist = whitelist
+            this.tagify.loading(false).dropdown.show(value)
+          })
       }
+    })
+  }
+
+  #onSelectSuggestion (event) {
+    if (event.detail.elm.classList.contains('tagify__dropdown__item__addNew')) {
+      this.tagify.addTags(this.tagify.state.inputText)
+      this.tagify.DOM.input.innerHTML = ''
     }
   }
 }
--- test/components/previews/ui/tag_input/component_preview.rb
+++ test/components/previews/ui/tag_input/component_preview.rb
@@ -2,10 +2,10 @@ class Ui::TagInput::ComponentPreview < ViewComponent::Preview
   # @param placeholder
   # @param default_value
   # @param whitelist
-  # @param maxItems number
+  # @param maxTags number
   # @param closeOnSelect toggle
 
-  def default(placeholder: "Choose your stack", default_value: "Ruby", whitelist: "Rails,Hotwire,Turbo,Stimulus,Tailwind,CSS,HTML,React,JS,Python,Svelte", maxItems: 10, closeOnSelect: false)
-    render(Ui::TagInput::Component.new(placeholder: placeholder, value: default_value, data: { whitelist: whitelist, maxItems: maxItems, closeOnSelect: closeOnSelect }))
+  def default(placeholder: "Choose your stack", default_value: "Ruby", whitelist: "Rails,Hotwire,Turbo,Stimulus,Tailwind,CSS,HTML,React,JS,Python,Svelte", maxTags: 100, closeOnSelect: false)
+    render(Ui::TagInput::Component.new(placeholder: placeholder, value: default_value, data: { whitelist: whitelist, maxTags: maxTags, closeOnSelect: closeOnSelect }))
   end
 end
