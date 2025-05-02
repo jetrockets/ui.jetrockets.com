956be4874a217f9f03c0315903c124c8278d380f
Valeryia Shyrko <valerka.shirko@gmail.com>
2025-05-02 15:46:10 +0200

    Tune drawer styles

--- test/components/previews/ui/drawer/component_preview.rb
+++ test/components/previews/ui/drawer/component_preview.rb
@@ -2,12 +2,12 @@ class Ui::Drawer::ComponentPreview < ViewComponent::Preview
   # @!group Sizes
 
   def drawer_trigger
-    render(Ui::Btn::Component.new(variant: :primary, data: { drawer_target: "trigger", drawer_toggle: "drawer", action: "click->drawer#show" }).with_content("Show drawer"))
+    render(Ui::Btn::Component.new(variant: :primary, data: { drawer_target: "trigger", drawer_toggle: "drawer", action: "click->drawer#show", id: "drawer-lookbook" }).with_content("Show drawer"))
   end
 
   def darawer(title: "Drawer Title", drawer_content: "Any amazing content")
-    render(Ui::Drawer::Component.new(title: title, async: false)) do
-      tag.div(drawer_content, class: "text-white")
+    render(Ui::Drawer::Component.new(title: title, async: false, id: "drawer-lookbook")) do
+      tag.div(drawer_content, class: "drawer__content")
     end
   end
 end
