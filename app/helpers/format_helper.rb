17f2e148e71c79ea3232b8467584a25902acbe88
Valeria Shirko <62662347+lera-sh@users.noreply.github.com>
2025-05-07 18:27:44 +0200

    Add datepicker (#27)
    * Add datepicker
    
    * Datepicker lookbook
    
    * Fix label
    
    * WIP
    
    * Tune styles
    
    * Move easepick from controller to utils
    
    ---------
    
    Co-authored-by: Alexey Solilin <solilin@jetrockets.com>

--- app/helpers/format_helper.rb
+++ app/helpers/format_helper.rb
@@ -1,5 +1,6 @@
 module FormatHelper
-  def date_format(date, format = :default)
-    date && I18n.l(date, format: format)
+  def date_format(date, format: :default, default: nil)
+    return default if date.blank?
+    I18n.l(date, format: format)
   end
 end
