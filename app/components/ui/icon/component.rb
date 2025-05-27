de02b2853bca30a3140f78a905497f159c4243f6
Valeryia Shyrko <valerka.shirko@gmail.com>
2025-05-27 17:18:55 +0200

    Remove tailwind.config.js file

--- app/components/ui/icon/component.rb
+++ app/components/ui/icon/component.rb
@@ -1,12 +1,26 @@
 class Ui::Icon::Component < ApplicationComponent
-  def initialize(icon_path:, **options)
+  SIZES = %i[4 6 8]
+  DEFAULT_SIZE = 4
+
+  def initialize(icon_path:, size: DEFAULT_SIZE, **options)
     @icon_path = icon_path
+    @size = size
     @options = options
   end
 
   def call
     with_asset_finder(InlineSvg::ViteAssetFinder) do
-      render_inline_svg(@icon_path, @options)
+      render_inline_svg(@icon_path, icon_classes)
     end
+  end
+
+  def icon_classes
+    {
+      class: class_names(
+        "h-#{@size} w-#{@size}",
+        @options.delete(:class)
+      ),
+      **@options
+    }
   end
 end
--- app/components/ui/tabs/item_component.rb
+++ app/components/ui/tabs/item_component.rb
@@ -13,7 +13,7 @@ class Ui::Tabs::ItemComponent < ApplicationComponent
     <%= link_to href, class: class_names('tabs__link', 'tabs__link-active': active), **options do %>
       <div class="flex items-center gap-2 w-max">
         <% if icon_path %>
-          <%= render Ui::Icon::Component.new(icon_path: icon_path, class: "w-4 h-4") %>
+          <%= render Ui::Icon::Component.new(icon_path: icon_path) %>
         <% end %>
         <%= @title %>
       </div>
