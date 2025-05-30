2c856653f0ccb22f3efb71cea629219f82e79a9d
Valeryia Shyrko <valerka.shirko@gmail.com>
2025-05-30 10:20:26 +0200

    Fix icons

--- app/components/ui/icon/component.rb
+++ app/components/ui/icon/component.rb
@@ -1,26 +1,12 @@
 class Ui::Icon::Component < ApplicationComponent
-  SIZES = %i[4 6 8]
-  DEFAULT_SIZE = 4
-
-  def initialize(icon_path:, size: DEFAULT_SIZE, **options)
+  def initialize(icon_path:, **options)
     @icon_path = icon_path
-    @size = size
     @options = options
   end
 
   def call
     with_asset_finder(InlineSvg::ViteAssetFinder) do
-      render_inline_svg(@icon_path, icon_classes)
+      render_inline_svg(@icon_path, @options)
     end
-  end
-
-  def icon_classes
-    {
-      class: class_names(
-        "h-#{@size} w-#{@size}",
-        @options.delete(:class)
-      ),
-      **@options
-    }
   end
 end
