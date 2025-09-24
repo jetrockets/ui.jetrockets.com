52bb9bce57899a047dd0e246c0816c73a98ade3b
Alexey Solilin <solilin@jetrockets.com>
2025-09-24 13:59:00 +0200

    Accordion

--- app/components/ui/accordion/item_component.rb
+++ app/components/ui/accordion/item_component.rb
@@ -9,13 +9,12 @@ class Ui::Accordion::ItemComponent < ApplicationComponent
   end
 
   erb_template <<~ERB
-    <button type="button" class="accordion__header" data-accordion-target="trigger" data-accordion-id="<%= @id %>" aria-expanded="<%= @opened %>">
+    <button type="button" class="<%= class_names('accordion__header', 'bg-gray-50': @opened) %>" data-accordion-target="trigger" data-accordion-id="<%= @id %>" aria-expanded="<%= @opened %>">
       <span>
         <%= @title %>
       </span>
-      <svg width="20px" data-accordion-icon class="w-3 h-3 transition-transform duration-300 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
-      </svg>
+
+      <%= helpers.vite_svg_tag('images/icons/chevron-down.svg', class: 'w-3 h-3 transition-transform duration-300 shrink-0', data: { accordion_icon: true }, aria: { hidden: true }) %>
     </button>
 
     <%= item %>
