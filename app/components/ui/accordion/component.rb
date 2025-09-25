fe94f8143b1ca5021d89c0576663d4bf0eb240b0
Alexey Solilin <solilin@jetrockets.com>
2025-09-25 22:36:00 +0200

    Tune Accordion

--- app/components/ui/accordion/component.rb
+++ app/components/ui/accordion/component.rb
@@ -1,3 +1,23 @@
+class Ui::Accordion::ItemComponent < ApplicationComponent
+  def initialize(name:, title: nil, open: false)
+    @name = name
+    @title = title
+    @open = open
+  end
+
+  erb_template <<~ERB
+    <details class="accordion__details" name="<%= @name %>" open="<%= @open %>">
+      <summary class="accordion__summary">
+        <%= @title %>
+        <%= helpers.vite_svg_tag('images/icons/chevron-down.svg', class: 'accordion__icon') %>
+      </summary>
+      <div class="accordion__body">
+        <%= content %>
+      </div>
+    </details>
+  ERB
+end
+
 class Ui::Accordion::Component < ApplicationComponent
   renders_many :items, Ui::Accordion::ItemComponent
 
@@ -7,19 +27,16 @@ class Ui::Accordion::Component < ApplicationComponent
   end
 
   erb_template <<~ERB
-    <%= content_tag :div, **attrs do %>
-      <div class="accordion" data-accordion="collapse">
-        <% items.each do |item| %>
-          <%= item %>
-        <% end %>
-      </div>
+    <%= content_tag :div, class: classes, **@options do %>
+      <% items.each do |item| %>
+        <%= item %>
+      <% end %>
     <% end %>
   ERB
 
   private
 
-  def attrs
-    data_attributes = ({ controller: "accordion" }).deep_merge(@options.fetch(:data, {}))
-    @options.merge(data: data_attributes)
+  def classes
+    class_names("accordion", @options.delete(:class))
   end
 end
