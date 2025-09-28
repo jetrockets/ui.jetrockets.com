87f05d55a66a256c15138b21c8ea86a61ff863c9
Alexey Solilin <solilin@jetrockets.com>
2025-09-28 11:52:42 +0200

    SIdebar scroll position

--- app/assets/controllers/sidebar_controller.js
+++ app/assets/controllers/sidebar_controller.js
@@ -1,10 +1,74 @@
 import { Controller } from '@hotwired/stimulus'
 import { stimulus } from '~/init'
 
+/* global sessionStorage */
+
 export default class Sidebar extends Controller {
-  static targets = ['menu', 'burger']
+  static targets = ['menu', 'burger', 'scroll']
   static values = {
     opened: { type: Boolean, default: false }
+  }
+
+  connect () {
+    this.#restoreScrollPosition()
+    this.#setupScrollListener()
+    this.#setupBeforeUnloadListener()
+  }
+
+  disconnect () {
+    if (this.#scrollDebounceTimer) {
+      clearTimeout(this.#scrollDebounceTimer)
+    }
+
+    if (this.hasScrollTarget && this.scrollTarget.scrollTop > 0) {
+      this.#saveScrollPosition()
+    }
+  }
+
+  #scrollDebounceTimer = null
+
+  #saveScrollPosition () {
+    if (!this.hasScrollTarget) return
+
+    const scrollPosition = this.scrollTarget.scrollTop
+    sessionStorage.setItem('sidebar-scroll-position', scrollPosition)
+  }
+
+  #saveScrollPositionDebounced () {
+    if (this.#scrollDebounceTimer) {
+      clearTimeout(this.#scrollDebounceTimer)
+    }
+
+    this.#scrollDebounceTimer = setTimeout(() => {
+      this.#saveScrollPosition()
+    }, 500)
+  }
+
+  #restoreScrollPosition () {
+    const savedPosition = sessionStorage.getItem('sidebar-scroll-position')
+
+    if (savedPosition && this.hasScrollTarget) {
+      const position = parseInt(savedPosition, 10)
+      this.scrollTarget.scrollTop = position
+    }
+  }
+
+  #setupScrollListener () {
+    if (!this.hasScrollTarget) return
+
+    this.scrollTarget.addEventListener('scroll', () => {
+      this.#saveScrollPositionDebounced()
+    })
+  }
+
+  #setupBeforeUnloadListener () {
+    document.addEventListener('turbo:before-visit', () => {
+      this.#saveScrollPosition()
+    })
+
+    window.addEventListener('beforeunload', () => {
+      this.#saveScrollPosition()
+    })
   }
 
   openedValueChanged (value) {
