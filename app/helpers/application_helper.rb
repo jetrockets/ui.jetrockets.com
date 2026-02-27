1a2c1e000de35a0e9bbf9fac80865cb703940dd6
Aleksei Solilin <solilin@jetrockets.com>
2026-02-27 10:03:03 +0100

    add picture_tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,10 +10,6 @@ module ApplicationHelper
     class_names("body", @body_class)
   end
 
-  def vite_asset_url_with_host(source)
-    URI.join(root_url, vite_asset_url(source))
-  end
-
   def vite_svg_tag(filename, transform_params = {})
     with_asset_finder(InlineSvg::ViteAssetFinder) do
       render_inline_svg(filename, transform_params)
@@ -23,6 +19,32 @@ module ApplicationHelper
   def vite_icon_tag(filename, transform_params = {})
     with_asset_finder(InlineSvg::ViteAssetFinder) do
       render_inline_svg("images/icons/#{filename}", transform_params)
+    end
+  end
+
+  # Picture tag
+  def vite_asset_exists?(logical_path)
+    File.exist?(Rails.root.join("app/assets", logical_path))
+  end
+
+  def vite_asset_url_with_host(source)
+    URI.join(root_url, vite_asset_url(source))
+  end
+
+  def source_tag(src)
+    tag.source(type: "image/avif", srcset: vite_asset_path(src))
+  end
+
+  # Use it in views like this: <%= picture_tag("images/example.jpg", alt: "Example Image") %>
+  def picture_tag(src, options = {})
+    avif_path = src.gsub(/\.(png|jpg|jpeg)$/, ".avif")
+
+    return unless vite_asset_exists?(src)
+
+    return vite_image_tag(src, **options) unless vite_asset_exists?(avif_path)
+
+    tag.picture do
+      source_tag(avif_path) + vite_image_tag(src, **options)
     end
   end
 
