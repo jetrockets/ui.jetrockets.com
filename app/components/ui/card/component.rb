fdce0d6c44d9e3ae7a9bae7b7b4d96b8d056c0c4
Aleksei Solilin <solilin@jetrockets.com>
2026-04-01 20:12:51 +0200

    Fix pagy

--- app/components/ui/card/component.rb
+++ app/components/ui/card/component.rb
@@ -11,7 +11,7 @@ class Ui::Card::Component < ApplicationComponent
 
   def classes
     class_names(
-      "border border-border bg-card rounded-card",
+      "border border-border bg-card rounded-card overflow-clip",
       @options.delete(:class)
     )
   end
